2e24807040f3ef42a7d398b76fce7740382d3323
Oasting Ferrera <austinmp215@hotmail.com>
2019-03-18 07:23:43 -0400

    Added error message

--- app/Http/Controllers/Api/Url/UrlController.php
+++ app/Http/Controllers/Api/Url/UrlController.php
@@ -29,7 +29,7 @@ class UrlController extends Controller
 			}
 
 		}catch( Exceptio $e ){
-			
+			Response::addMessage( 'Error creating short URL, please try again' , 'error' );
 		}
 		Response::flush( );
 	}
